88c09a540c5ab11ada39f9d4c3ba78185c5c8a40
waynelee-lwc <1418551940@qq.com>
2023-04-03 17:24:05 +0800

    tag apis

--- github.com/jlu-cow-studio/common/dal/rpc/tag_core/tag_core_grpc.pb.go
+++ github.com/jlu-cow-studio/common/dal/rpc/tag_core/tag_core_grpc.pb.go
@@ -23,6 +23,10 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type TagCoreServiceClient interface {
 	GetTagListByScene(ctx context.Context, in *GetTagListBySceneRequest, opts ...grpc.CallOption) (*GetTagListBySceneResponse, error)
+	GetTagListByItem(ctx context.Context, in *GetTagListByItemRequest, opts ...grpc.CallOption) (*GetTagListByItemResponse, error)
+	GetTagListByUser(ctx context.Context, in *GetTagListByUserRequest, opts ...grpc.CallOption) (*GetTagListByUserResponse, error)
+	UpdateItemTags(ctx context.Context, in *UpdateItemTagsRequest, opts ...grpc.CallOption) (*UpdateItemTagsResponse, error)
+	UpdateUserTags(ctx context.Context, in *UpdateUserTagsRequest, opts ...grpc.CallOption) (*UpdateUserTagsResponse, error)
 }
 
 type tagCoreServiceClient struct {
@@ -42,11 +46,51 @@ func (c *tagCoreServiceClient) GetTagListByScene(ctx context.Context, in *GetTag
 	return out, nil
 }
 
+func (c *tagCoreServiceClient) GetTagListByItem(ctx context.Context, in *GetTagListByItemRequest, opts ...grpc.CallOption) (*GetTagListByItemResponse, error) {
+	out := new(GetTagListByItemResponse)
+	err := c.cc.Invoke(ctx, "/jlu_cow_studio.tag_core.TagCoreService/GetTagListByItem", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *tagCoreServiceClient) GetTagListByUser(ctx context.Context, in *GetTagListByUserRequest, opts ...grpc.CallOption) (*GetTagListByUserResponse, error) {
+	out := new(GetTagListByUserResponse)
+	err := c.cc.Invoke(ctx, "/jlu_cow_studio.tag_core.TagCoreService/GetTagListByUser", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *tagCoreServiceClient) UpdateItemTags(ctx context.Context, in *UpdateItemTagsRequest, opts ...grpc.CallOption) (*UpdateItemTagsResponse, error) {
+	out := new(UpdateItemTagsResponse)
+	err := c.cc.Invoke(ctx, "/jlu_cow_studio.tag_core.TagCoreService/UpdateItemTags", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *tagCoreServiceClient) UpdateUserTags(ctx context.Context, in *UpdateUserTagsRequest, opts ...grpc.CallOption) (*UpdateUserTagsResponse, error) {
+	out := new(UpdateUserTagsResponse)
+	err := c.cc.Invoke(ctx, "/jlu_cow_studio.tag_core.TagCoreService/UpdateUserTags", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // TagCoreServiceServer is the server API for TagCoreService service.
 // All implementations must embed UnimplementedTagCoreServiceServer
 // for forward compatibility
 type TagCoreServiceServer interface {
 	GetTagListByScene(context.Context, *GetTagListBySceneRequest) (*GetTagListBySceneResponse, error)
+	GetTagListByItem(context.Context, *GetTagListByItemRequest) (*GetTagListByItemResponse, error)
+	GetTagListByUser(context.Context, *GetTagListByUserRequest) (*GetTagListByUserResponse, error)
+	UpdateItemTags(context.Context, *UpdateItemTagsRequest) (*UpdateItemTagsResponse, error)
+	UpdateUserTags(context.Context, *UpdateUserTagsRequest) (*UpdateUserTagsResponse, error)
 	mustEmbedUnimplementedTagCoreServiceServer()
 }
 
@@ -56,6 +100,18 @@ type UnimplementedTagCoreServiceServer struct {
 
 func (UnimplementedTagCoreServiceServer) GetTagListByScene(context.Context, *GetTagListBySceneRequest) (*GetTagListBySceneResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetTagListByScene not implemented")
+}
+func (UnimplementedTagCoreServiceServer) GetTagListByItem(context.Context, *GetTagListByItemRequest) (*GetTagListByItemResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetTagListByItem not implemented")
+}
+func (UnimplementedTagCoreServiceServer) GetTagListByUser(context.Context, *GetTagListByUserRequest) (*GetTagListByUserResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetTagListByUser not implemented")
+}
+func (UnimplementedTagCoreServiceServer) UpdateItemTags(context.Context, *UpdateItemTagsRequest) (*UpdateItemTagsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateItemTags not implemented")
+}
+func (UnimplementedTagCoreServiceServer) UpdateUserTags(context.Context, *UpdateUserTagsRequest) (*UpdateUserTagsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateUserTags not implemented")
 }
 func (UnimplementedTagCoreServiceServer) mustEmbedUnimplementedTagCoreServiceServer() {}
 
@@ -88,6 +144,78 @@ func _TagCoreService_GetTagListByScene_Handler(srv interface{}, ctx context.Cont
 	return interceptor(ctx, in, info, handler)
 }
 
+func _TagCoreService_GetTagListByItem_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetTagListByItemRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TagCoreServiceServer).GetTagListByItem(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/jlu_cow_studio.tag_core.TagCoreService/GetTagListByItem",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TagCoreServiceServer).GetTagListByItem(ctx, req.(*GetTagListByItemRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _TagCoreService_GetTagListByUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetTagListByUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TagCoreServiceServer).GetTagListByUser(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/jlu_cow_studio.tag_core.TagCoreService/GetTagListByUser",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TagCoreServiceServer).GetTagListByUser(ctx, req.(*GetTagListByUserRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _TagCoreService_UpdateItemTags_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateItemTagsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TagCoreServiceServer).UpdateItemTags(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/jlu_cow_studio.tag_core.TagCoreService/UpdateItemTags",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TagCoreServiceServer).UpdateItemTags(ctx, req.(*UpdateItemTagsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _TagCoreService_UpdateUserTags_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateUserTagsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TagCoreServiceServer).UpdateUserTags(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/jlu_cow_studio.tag_core.TagCoreService/UpdateUserTags",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TagCoreServiceServer).UpdateUserTags(ctx, req.(*UpdateUserTagsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // TagCoreService_ServiceDesc is the grpc.ServiceDesc for TagCoreService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +226,22 @@ var TagCoreService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetTagListByScene",
 			Handler:    _TagCoreService_GetTagListByScene_Handler,
+		},
+		{
+			MethodName: "GetTagListByItem",
+			Handler:    _TagCoreService_GetTagListByItem_Handler,
+		},
+		{
+			MethodName: "GetTagListByUser",
+			Handler:    _TagCoreService_GetTagListByUser_Handler,
+		},
+		{
+			MethodName: "UpdateItemTags",
+			Handler:    _TagCoreService_UpdateItemTags_Handler,
+		},
+		{
+			MethodName: "UpdateUserTags",
+			Handler:    _TagCoreService_UpdateUserTags_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
